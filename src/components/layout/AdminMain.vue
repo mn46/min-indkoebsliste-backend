1b035f69474bc74cf792c855c8c279e84058dead
Sofie <sov.002@edu.zealand.dk>
2025-11-23 18:48:32 +0100

    AdminMain + Assets folder adeed

---
+++ src/components/layout/AdminMain.vue
@@ -0,0 +1,32 @@
+<template>
+   <div :class="['main-layout', backgroundClass]">
+    <TheHeaderAdmin/> 
+    <main>
+<slot/>
+
+    </main>  
+      <TheFooter/>
+
+</template>
+
+
+<script>
+import TheHeaderAdmin from '@/components/layout/TheHeaderAdmin.vue';
+import TheFooter from '@/components/layout/FooterAdmin.vue';
+export default{
+    name: 'MainLayout',
+    components:{
+        TheHeaderAdmin, TheFooter
+    },
+    props:{
+        backgroundClass:{
+            type:String,
+            default:''
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
+<!-- “Her er min header, footer og baggrund. Der, hvor <slot> står, sætter jeg alt indhold fra den side, der bruger layoutet. -->
