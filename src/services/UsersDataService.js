c7bd9e5237e12dc2f521ae005da95adbe1d5e208
marta <m.nowak033@gmail.com>
2025-11-25 22:33:19 +0100

    Add withCredentials to allow cookies

--- src/services/UsersDataService.js
+++ src/services/UsersDataService.js
@@ -1,13 +1,13 @@
 import http from "../http-common";
 
 class UsersDataService {
-    signup(data){
-        return http.post("/users/sign-up", data );
-    }
-    signin(data){
-        return http.post("/users/sign-in", data);
-    }
-    
-
+  signup(data) {
+    return http.post("/users/sign-up", data);
+  }
+  signin(data) {
+    return http.post("/users/sign-in", data, {
+      withCredentials: true,
+    });
+  }
 }
 export default new UsersDataService();
